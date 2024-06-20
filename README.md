d6ca2067eef0277adef029cbc0659688e17a00d1
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2024-06-19 22:23:48 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Balacobaco ORM
+
+Projeto Final Desenvolvido para a matéria de Programação Orientada a Objeto.
+
+## Descrição
+
+Balacobaco ORM é um projeto desenvolvido como trabalho final para a disciplina de Programação Orientada a Objeto.
+
+## Repositório
+
+O código fonte do projeto está disponível no GitHub:
+[balacobaco-orm](https://github.com/luizpaulo2005/balacobaco-orm)
+
+## Contribuidores
+
+- **Luiz Paulo R. S. Santos**
+  - Email: [luizpaulorodrigues2005@gmail.com](mailto:luizpaulorodrigues2005@gmail.com)
+  - GitHub: [luizpaulo2005](https://github.com/luizpaulo2005)
+
+- **Vitor de Souza Lichoti**
+  - Email: [vitorlichoti@gmail.com](mailto:vitorlichoti@gmail.com)
+  - GitHub: [vitorlichoti](https://github.com/vitorlichoti)
+
+## Informações Adicionais
+
+- **Versão:** 1.0.0
+- **Arquivo Principal:** `index.js`
+- **Tipo de Módulo:** ES Module
+- **Licença:** MIT
---
+++ index.js
@@ -0,0 +1,94 @@
+class ORM {
+  #lista;
+
+  constructor() {
+    if (this.constructor === ORM) {
+      throw new Error("Objeto ORM não pode ser instânciado");
+    }
+    this.#lista = [];
+  }
+  
+  #createItem(item) {
+    const qtd = this.#lista.length;
+    if (item != undefined || item != null) {
+      if (qtd == 0) {
+        this.#lista.push({
+          id: 0,
+          ...item,
+        });
+      } else {
+        this.#lista.push({
+          id: this.#lista[qtd - 1].id + 1,
+          ...item,
+        });
+      }
+      return "Item adicionado";
+    }
+  }
+
+  create(item) {
+    return this.#createItem(item);
+  }
+
+  #selecionarTodos() {
+    if (this.#lista.length == 0) {
+      return `Lista de ${this.constructor.name}(s) vazia`;
+    }
+
+    return this.#lista.map((item) => {
+      return JSON.stringify(item);
+    });
+  }
+
+  get selectAll() {
+    return this.#selecionarTodos();
+  }
+
+  #selecionarUnico(id) {
+    if (!this.#lista.find((item) => item.id == id)) {
+      return `Item ${id} não encontrado`;
+    }
+
+    return this.#lista.find((item) => item.id == id);
+  }
+
+  selectUnique(id) {
+    return this.#selecionarUnico(id);
+  }
+
+  #atualizar(id, item) {
+    const obj = this.#lista.find((item) => item.id == id);
+    if (!obj) {
+      return `Item ${id} não encontrado`;
+    }
+
+    const index = this.#lista.indexOf(obj);
+
+    this.#lista[index] = {
+      id,
+      ...item,
+    };
+    return `Item ${id} atualizado`;
+  }
+
+  update(id, item) {
+    return this.#atualizar(id, item);
+  }
+
+  #apagar(id) {
+    const item = this.#lista.find((item) => item.id == id);
+    if (!item) {
+      return `Item ${id} não encontrado`;
+    }
+    const index = this.#lista.indexOf(item);
+    this.#lista.splice(index, 1);
+
+    return `Item ${id} excluído`;
+  }
+
+  delete(id) {
+    return this.#apagar(id);
+  }
+}
+
+export { ORM };
